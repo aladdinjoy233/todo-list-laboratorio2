ecda693453611737d326a55f2dcd6f85e33f850c
Aladdin <ninjalover249@gmail.com>
2022-11-08 18:34:15 -0300

    ADD - Todo controller

--- routes/todo.js
+++ routes/todo.js
@@ -2,240 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 var authController = require('../controllers/authController');
-
-var Lista = require('../database/models/Lista');
-var Tarea = require('../database/models/Tarea');
-var { Op } = require('sequelize');
+var todoController = require('../controllers/todoController');
 
 // Main todo page
-router.get('/', authController.isAuthenticated, async (req, res, next) => {
-	await actualizarTareas();
-
-	const {listas, tareas} = await obtenerTareas();
-
-	res.render('todo', { title: 'My todo list | List', tareas, listas });
-});
-
-router.get('/obtener', async (req, res, next) => {
-	const { listas, tareas } = await obtenerTareas();
-	
-	// res.send(JSON.stringify({listas, tareas}));
-	res.json({listas, tareas});
-});
+router.get('/', authController.isAuthenticated, todoController.index)
 
 // POST para cambiar el estado de la tarea (resuelto o pendiente)
-router.post('/cambiar_estado/:id', async (req, res, next) => {
-	let id = req.params.id;
-	const tarea = await Tarea.findByPk(id);
-
-	if (tarea.fechaLimite) {
-		const fechaTarea = new Date(tarea.fechaLimite);
-		const hoy = new Date();
-
-		if (fechaTarea < hoy) {
-			res.json(null);
-			return;
-		}
-	}
-	await cambiarEstado(tarea);
-
-	if (tarea.listaId == null) {
-		res.json({tarea, lista: null});
-		return;
-	}
-
-	const lista = await Lista.findByPk(tarea.listaId, {
-		include: [{
-			model: Tarea,
-			as: "tareas"
-		}]
-	});
-
-	let estadoCompleto = tarea.estado == "resuelto" ? true : false;
-
-	if (!estadoCompleto && lista.estado != 'pendiente') {
-		lista.estado = 'pendiente';
-		lista.resolucion = null;
-		await lista.save();
-		res.json({tarea, lista});
-		return;
-	}
-
-	let todasCompletas = true;
-
-	for (const liTarea of lista.tareas) {
-		if (liTarea.estado != 'resuelto') {
-			todasCompletas = false;
-			break;
-		}
-	}
-
-	if (todasCompletas && lista.estado != 'resuelto') {
-		lista.estado = 'resuelto';
-		lista.resolucion = new Date();
-	}
-	await lista.save();
-
-	res.json({tarea, lista});
-});
+router.post('/cambiar_estado/:id', authController.isAuthenticated, todoController.cambiarEstado)
 
 // POST para agregar una lista de tareas
-router.post('/add_lista', async (req, res, next) => {
-	await Lista.create({
-		titulo: req.body.nombreLista,
-		creacion: new Date(),
-		resolucion: null,
-		estado: 'pendiente'
-	});
-
-	res.send({redirect: '/todo'});
-});
+router.post('/add_lista', authController.isAuthenticated, todoController.agregarLista)
 
 // POST para agregar una tarea
-router.post('/add_tarea', async (req, res, next) => {
-
-	let { data } = req.body;
-
-	await Tarea.create({
-		titulo: data.titulo,
-		creacion: new Date(),
-		resolucion: null,
-		descripcion: data.descripcion ? data.descripcion : null,
-		prioridad: data.prioridad,
-		fechaLimite: data.limite ? data.limite : null,
-		estado: 'pendiente',
-		listaId: data.lista == 'null' ? null : data.lista
-	});
-
-	res.send({redirect: '/todo'});
-});
-
-// POST para borrar una tarea
-router.post('/delete_tarea/:id', async (req, res, next) => {
-	let id = req.params.id;
-
-	const tarea = await Tarea.findByPk(id);
-
-	await tarea.destroy();
-
-	res.send({redirect: '/todo'});
-});
+router.post('/add_tarea', authController.isAuthenticated, todoController.agregarTarea)
 
 // POST para borrar una lista
-router.post('/delete_lista/:id', async (req, res, next) => {
-	let id = req.params.id;
+router.post('/delete_lista/:id', authController.isAuthenticated, todoController.borrarLista)
 
-	const lista = await Lista.findByPk(id);
-	const tareas = await Tarea.findAll({ where: { listaId: lista.id	} });
-
-	if (tareas.length > 0) {
-		for (const tarea of tareas) {
-			await tarea.destroy();
-		}
-	}
-
-	await lista.destroy();
-	res.send({redirect: '/todo'});
-});
-
-
-// Funciones
-async function obtenerTareas() {
-	// Trae todas las listas con sus tareas
-	const listas = await Lista.findAll({
-		include: [{
-			model: Tarea,
-			as: "tareas"
-		}],
-		order: [
-			[{ model: Tarea, as: 'tareas' }, 'prioridad', 'ASC']
-		]
-
-	});
-
-	// Trae las demas tareas sin corresponder a una lista
-	const tareas = await Tarea.findAll({
-		where: {
-			listaId: null
-		},
-		order: [
-			['prioridad', 'ASC']
-		]
-	})
-
-	return { listas, tareas };
-}
-
-async function actualizarTareas() {
-	const tareas = await Tarea.findAll();
-
-	let hoy = new Date();
-
-	for (const tarea of tareas) {
-		if (tarea.fechaLimite == null || tarea.estado == "resuelto") {
-			continue;
-		}
-		const fechaTarea = new Date(tarea.fechaLimite);
-	
-		if (fechaTarea < hoy) {
-			await cambiarEstado(tarea, fechaTarea);
-		}
-		
-	}
-
-	const listas = await Lista.findAll({
-		include: [{
-			model: Tarea,
-			as: "tareas"
-		}]
-	});
-
-	for (const lista of listas) {
-
-		if (lista.tareas.length == 0) {
-			if (lista.estado == 'resuelto') {
-				lista.estado = 'pendiente';
-				lista.resolucion = null;
-				lista.save();
-			}
-			continue;
-		}
-
-		let todasCompletas = true;
-
-		for (const liTarea of lista.tareas) {
-			if (liTarea.estado != 'resuelto') {
-				todasCompletas = false;
-			}
-		}
-
-		if (todasCompletas && lista.estado != 'resuelto') {
-			lista.estado = 'resuelto';
-			lista.resolucion = hoy;
-		}
-		
-		if (!todasCompletas && lista.estado != 'pendiente') {
-			lista.estado = 'pendiente';
-			lista.resolucion = null;
-		}
-
-		await lista.save();
-
-	}
-}
-
-async function cambiarEstado(tarea, date = new Date()) {
-
-	if (tarea.estado == "pendiente") {
-		tarea.estado = "resuelto";
-		tarea.resolucion = date;
-	} else {
-		tarea.estado = "pendiente";
-		tarea.resolucion = null;
-	}
-
-	await tarea.save();
-	return tarea;
-}
+// POST para borrar una tarea
+router.post('/delete_tarea/:id', authController.isAuthenticated, todoController.borrarTarea)
 
 module.exports = router;
